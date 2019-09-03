5169a7034a09b767597c1974bbc96aeb6c6813de
shubham860 <shubham.chauhan860@gmail.com>
2019-09-03 21:40:36 +0530

    update

---
+++ portfolio/src/components/Dashboard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../css/Dashboard.css'
+import Info from './components/Info'
+
+
+class Dashboard extends React.Component {
+  render () {
+      return(
+            <div class='container'>
+              <div class='item1'>
+                <Info/>
+              </div>
+              <div class='item2'>
+
+              </div>
+            </div>
+      )
+  }
+}
+
+export default Dashboard;
---
+++ portfolio/src/components/Info.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Info extends React.Component {
+  render () {
+        return(
+          bhgchcjh
+        )
+  }
+}
+
+export default Info;
