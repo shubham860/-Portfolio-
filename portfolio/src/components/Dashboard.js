1a9eb2d9df0481fe0851e7a8b50f2a21755db7ab
shubham860 <shubham.chauhan860@gmail.com>
2019-09-07 02:28:59 +0530

    update

--- portfolio/src/components/Dashboard.js
+++ portfolio/src/components/Dashboard.js
@@ -14,34 +14,50 @@ class Dashboard extends React.Component {
   render () {
       return(
             <div class='container'>
+
               <div class='item1'>
                 <Info/>
               </div>
+
               <div class='item2'>
-                <div class='item2a'>
-                  <div class='Resume'>
-                  <div class='one'><p class='onep'>01</p></div>
-                  <Link to='/Resume' class='deco'><p class='Specs'>RESUME</p></Link>
+
+                <Link to='/Resume' class='deco'>
+                  <div class='item2a'>
+                    <div class='Resume'>
+                    <div class='one'><p class='onep'>01</p></div>
+                    <p class='Specs'>RESUME</p>
+                    </div>
                   </div>
-                </div>
+                </Link>
+
+
+              <Link to='/Portfolio' class='deco'>
                 <div class='item2b'>
                   <div class='Portfolio'>
                     <div class='two'><p class='twop'>02</p></div>
-                    <Link to='/Portfolio' class='deco'><p class='Specs'>PORTFOLIO</p></Link>
+                    <p class='Specs'>PORTFOLIO</p>
                   </div>
                 </div>
+              </Link>
+
+              <Link to='/Skills' class='deco'>
                 <div class='item2c'>
                   <div class='Skills'>
                     <div class='two'><p class='twop'>03</p></div>
-                    <Link to='/Skills' class='deco'><p class='Specs'>SKILLS</p></Link>
+                    <p class='Specs'>SKILLS</p>
                   </div>
                 </div>
+              </Link>
+
+              <Link to='/Contact' class='deco'>
                 <div class='item2d'>
                   <div class='Contact'>
                     <div class='two'><p class='twop'>04</p></div>
-                    <Link to='/Contact' class='deco'><p class='Specs'>CONTACT</p></Link>
+                    <p class='Specs'>CONTACT</p>
                   </div>
                 </div>
+              </Link>  
+
               </div>
             </div>
       )
