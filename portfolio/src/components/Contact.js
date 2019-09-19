9e2ff18890f06a53ac52a16d3ea64d435c17dbf8
shubham860 <shubham.chauhan860@gmail.com>
2019-09-19 11:33:37 +0530

    update

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -5,7 +5,7 @@ import Nav from '../components/Nav'
 import '../css/Contact.css'
 
 class Contact extends React.Component {
-  
+
   constructor(){
     super()
     this.state= {
@@ -17,7 +17,7 @@ class Contact extends React.Component {
 
   handler1 = (event) => {
       this.setState({
-        name : event.target.value
+        [event.target.name] : event.target.value
       })
   }
 
@@ -34,9 +34,19 @@ class Contact extends React.Component {
   }
 
   submit = () => {
-    alert(` Name : ${this.state.name}`)
-  }
+   const { name,email,text} = this.state
 
+   if(name=='' | email =='' | text=='' )
+   {
+     alert(`Enter All inputs properly`);
+   }
+   else
+   {
+     alert(`Name : ${name}\nEmail : ${email}\nMessage : {text}`);
+   }
+
+
+}
   render () {
     const {name,email,text} = this.state
     return(
@@ -53,10 +63,10 @@ class Contact extends React.Component {
 
                 <div class='form'>
                   <div class='inputs'>
-                    <input type='text' class='ip' value={name} onChange={this.handler1} /><br/>
-                    <input type='email' class='ip' value={email} onChange={this.handler2}  /><br/>
-                    <textarea value={text} onChange={this.handler3} ></textarea><br/>
-                    <button type='button' onClick={this.submit}>Submit</button>
+                    <input type='text' class='ip' name='name' value={name} onChange={this.handler1} placeholder="Name*" autoComplete='OFF'/><br/>
+                    <input type='email' class='ip' value={email} onChange={this.handler2} placeholder="Email*"  /><br/>
+                    <textarea value={text} class='tb' onChange={this.handler3} placeholder="Your Message*" ></textarea><br/>
+                    <button type='button' class='btn' onClick={this.submit}>SEND MESSAGE</button>
                   </div>
                   <div class='address'>
                     <h1>hello</h1>
