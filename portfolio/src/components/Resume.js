60fa335d14791d9823642db8dc436dbe946d385d
shubham860 <shubham.chauhan860@gmail.com>
2019-09-22 18:54:04 +0530

    UPDATE

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -19,8 +19,30 @@ class Resume extends React.Component {
               </div>
 
               <div class='intro'>
-                <p class='intro-lines'>I'm a Frontend Developer and  Machine Learning enthusiast from Moradabad, UP. I aim to make a difference through my creative solution.</p>
+                <p class='intro-lines'>I'm a <span class='work'>Frontend Developer &  Machine Learning enthusiast</span> from Moradabad, UP. I aim to make a difference through my creative solution.</p>
               </div>
+
+              <div class='about-intro'>
+                <div class='lines'>
+                  <p class='options-details'><span class='options'>Name :  </span> Shubham Chauhan</p>
+                  <p class='options-details'><span class='options'>Date of birth :  </span> September 28, 1997</p>
+                </div>
+
+                <div class='lines'>
+                  <p class='options-details'><span class='options'>Address :  </span> Street no. 2 Line Par majhola, Moradabad</p>
+                  <p class='options-details'><span class='options'>Email :  </span> <a href='mailto: shubham.chauhan860@gmail.com' class='tel'>Shubham.chauhan860@gmail.com</a></p>
+                </div>
+
+                <div class='lines'>
+                  <p class='options-details'><span class='options'>Mobile no :  </span> <a href='tel:+918791555171' class='tel'>+918791555171</a></p>
+                  <p class='options-details'><span class='options'>Skype :  </span> Shubham860</p>
+                </div>
+
+                <div class='about-head' style={{marginTop : "40px"}}>
+                  <p class='chead'>Skills</p>
+                </div>
+              </div>
+
             </div>
           </div>
 
