af5ee401fa073bdacbda0088fd59df0ee0680b23
shubham860 <shubham.chauhan860@gmail.com>
2019-09-23 22:19:00 +0530

    update

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Info_dup from '../components/Info_dup'
 import Nav from '../components/Nav'
 import '../css/Portfolio.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 class Portfolio extends React.Component {
   render () {
@@ -16,6 +17,9 @@ class Portfolio extends React.Component {
               <div class='start'>
                 <div class='about-head'>
                   <p class='chead'>PORTFOLIO</p>
+                </div>
+                <div class='filters'>
+
                 </div>
               </div>
           </div>
