bcedcd15963ac8b69a429869362b1a2a480cc1bd
shubham860 <shubham.chauhan860@gmail.com>
2019-09-24 00:35:32 +0530

    update

--- portfolio/src/components/Nav2.js
+++ portfolio/src/components/Nav2.js
@@ -9,9 +9,9 @@ class Nav2 extends React.Component {
   render () {
     return(
         <ul class='nav-links'>
-          <li><Link to='/Portfolio/All'><p class='nav-a'>All</p></Link></li>
-          <li><Link to='/Portfolio/Frontend'><p class='nav-a'>Frontend</p></Link></li>
-          <li><Link to='/Portfolio/Ml'><p class='nav-a'>Machine Learning</p></Link></li>
+          <li><Link to='/Portfolio/All'><p class='nav-b'>All</p></Link></li>
+          <li><Link to='/Portfolio/Frontend'><p class='nav-b'>Frontend</p></Link></li>
+          <li><Link to='/Portfolio/Ml'><p class='nav-b'>Machine Learning</p></Link></li>
         </ul>
     )
   }
