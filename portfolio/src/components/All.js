2735b44a7f4b2032c3d1fa82d14379ebf0f8a983
shubham860 <shubham.chauhan860@gmail.com>
2019-10-07 00:12:49 +0530

    update

--- portfolio/src/components/All.js
+++ portfolio/src/components/All.js
@@ -4,31 +4,134 @@ import '../css/Portfolio.css'
 import engilife from '../images/engilife.png'
 import alumni from '../images/alumni.png'
 import radar from '../images/Radar.png'
+import reacthub from '../images/reacthub.png'
+import weather from '../images/weather.png'
+import book from '../images/bookmarker.png'
+import aarambh from '../images/aarambh.png'
+import todo from '../images/todo.png'
+import homie from '../images/Homie.png'
+
+
 class All extends React.Component {
   render () {
     return(
+      <div>
       <div class='all'>
         <div class='image-container' >
           <img src={engilife} class='port-image'/>
             <div class="middle">
-               <div class="text"> <a href="https://www.w3schools.com/css/css3_2dtransforms.asp" target="_blank">View</a> </div>
+              <div>
+                <h4>EngiLife</h4>
+              </div>
+               <div class="text">
+                 <a href="https://www.engilife.com/" target="_blank">Visit</a>
+               </div>
               </div>
         </div>
 
         <div class='image-container' >
-          <img src={engilife} class='port-image'/>
+          <img src={alumni} class='port-image'/>
             <div class="middle">
-               <div class="text"><a href="https://www.w3schools.com/css/css3_2dtransforms.asp" target="_blank">View</a> </div>
+              <div>
+                <h4>MIT alumni</h4>
+              </div>
+               <div class="text">
+                 <a href="https://mitalumni.in/" target="_blank">Visit</a>
+               </div>
               </div>
         </div>
 
         <div class='image-container' >
-          <img src={engilife} class='port-image'/>
+          <img src={radar} class='port-image'/>
             <div class="middle">
-               <div class="text"> <a href="https://www.w3schools.com/css/css3_2dtransforms.asp" target="_blank">View</a> </div>
+              <div>
+                <h4>Dashboard</h4>
+              </div>
+               <div class="text">
+                 <a href="https://reactassignment.surge.sh/" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+
+      </div>
+
+      <div class='all'>
+        <div class='image-container' >
+          <img src={reacthub} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>ReactHub</h4>
+              </div>
+               <div class="text">
+                 <a href="https://reacthub.surge.sh/" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={weather} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Weather app</h4>
+              </div>
+               <div class="text">
+                 <a href="https://scweather.surge.sh/" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={book} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Webmarker</h4>
+              </div>
+               <div class="text">
+                 <a href="https://webmarker.surge.sh/" target="_blank">Visit</a>
+               </div>
               </div>
         </div>
       </div>
+
+      <div class='all'>
+        <div class='image-container' >
+          <img src={aarambh} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Aarambh TSI</h4>
+              </div>
+               <div class="text">
+                 <a href="https://aarambh.surge.sh/" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={todo} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Todo List</h4>
+              </div>
+               <div class="text">
+                 <a href="https://sctodo.surge.sh" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={homie} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Homie</h4>
+              </div>
+               <div class="text">
+                 <a href="https://reactassignment.surge.sh/" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+      </div>
+
+    </div>
     )
   }
 }
--- portfolio/src/components/ML.js
+++ portfolio/src/components/ML.js
@@ -1,11 +1,132 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import '../css/Portfolio.css'
+import mnist from '../images/mnist.png'
+import election from '../images/election.png'
+import movie from '../images/movie.jpeg'
+import usa from '../images/usa.png'
+import review1 from '../images/a_review.png'
+
 class Ml extends React.Component {
   render () {
     return(
       <div>
-      <h1>Hello ML</h1>
+      <div class='all'>
+        <div class='image-container' >
+          <img src={mnist} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Digit Recogniser</h4>
+              </div>
+               <div class="text">
+                 <a href='https://github.com/shubham860' target='_blank'><ion-icon name="logo-github" class='logo'></ion-icon></a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={election} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Lok-sabha 2019</h4>
+              </div>
+               <div class="text">
+                  <a href='https://github.com/shubham860' target='_blank'><ion-icon name="logo-github" class='logo'></ion-icon></a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={movie} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Movie Recommendation</h4>
+              </div>
+               <div class="text">
+                <a href='https://github.com/shubham860' target='_blank'><ion-icon name="logo-github" class='logo'></ion-icon></a>
+               </div>
+              </div>
+        </div>
+
       </div>
+
+      <div class='all'>
+        <div class='image-container' >
+          <img src={usa} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>USA Military Bases</h4>
+              </div>
+               <div class="text">
+                 <a href='https://github.com/shubham860' target='_blank'><ion-icon name="logo-github" class='logo'></ion-icon></a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={review1} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>AT&T Reviews Classifier</h4>
+              </div>
+               <div class="text">
+                 <a href='https://github.com/shubham860' target='_blank'><ion-icon name="logo-github" class='logo'></ion-icon></a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={book} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Webmarker</h4>
+              </div>
+               <div class="text">
+                 <a href="https://webmarker.surge.sh/" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+      </div>
+
+      <div class='all'>
+        <div class='image-container' >
+          <img src={aarambh} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Aarambh TSI</h4>
+              </div>
+               <div class="text">
+                 <a href="https://aarambh.surge.sh/" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={todo} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Todo List</h4>
+              </div>
+               <div class="text">
+                 <a href="https://sctodo.surge.sh" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+
+        <div class='image-container' >
+          <img src={homie} class='port-image'/>
+            <div class="middle">
+              <div>
+                <h4>Homie</h4>
+              </div>
+               <div class="text">
+                 <a href="https://reactassignment.surge.sh/" target="_blank">Visit</a>
+               </div>
+              </div>
+        </div>
+      </div>
+
+    </div>
     )
   }
 }
