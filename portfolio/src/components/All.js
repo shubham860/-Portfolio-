fb92485763f8b3ed43afe251a8bd037f5c47b0b3
shubham860 <shubham.chauhan860@gmail.com>
2019-10-06 21:38:52 +0530

    update

--- portfolio/src/components/All.js
+++ portfolio/src/components/All.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import '../css/Portfolio.css'
+import engilife from '../images/engilife.png'
+import alumni from '../images/alumni.png'
+import radar from '../images/Radar.png'
 class All extends React.Component {
   render () {
     return(
-      <div>
-      <h1>Hello All</h1>
+      <div class='all'>
+        <div>
+          <img src={engilife} class='small'/>
+        </div>
+        <div>
+          <img src={alumni} class='small'/>
+        </div>
+        <div>
+          <img src={radar} class='small'/>
+        </div>
       </div>
     )
   }
